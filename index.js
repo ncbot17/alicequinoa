102b3e80657e931eecf63b3cc59a013cc1f10512
ncbot17 <ncbot17@gmail.com>
2017-10-04 15:16:08 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -62,6 +62,8 @@ app.post('/webhook', function (req, res) {
   function receivedMessage(event) {
     // Putting a stub for now, we'll expand it in the following steps
     console.log("Message data: ", event.message);
+    console.log("FB_VERIFY_TOKEN: " + token);
+    console.log("FB_ACCESS_TOKEN: " + access);
   }
 
 app.listen(app.get('port'), function(){
