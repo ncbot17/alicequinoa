72d884cf18da64ca5d41d013f1e0e2928cbaa957
ncbot17 <ncbot17@gmail.com>
2017-10-04 08:36:24 +0200

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const bodyParser = require('body-parser');
 const request = require('request');
 
 const app = express();
-app.set('port', (process.env.port||6700));
+app.set('port', (process.env.PORT||6700));
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
