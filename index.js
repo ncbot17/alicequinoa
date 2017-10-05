e00314987eee8c2f70a987849e5c73f026b1f39d
ncbot17 <ncbot17@gmail.com>
2017-10-05 11:40:56 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(bodyParser.json());
 app.get('/', function(req,res){
     res.send('Hello Youtube!')
 })
-
+/*
 app.get('/webhook/', function(req, res){
     console.log(req.query);
     if(req.query['hub.verify_token'] ===
@@ -28,45 +28,109 @@ app.get('/webhook/', function(req, res){
         }
     res.send('No entry')
 })
+*/
+
+app.get('/webhook', function(req, res) {
+  if (req.query['hub.mode'] === 'subscribe' &&
+      req.query['hub.verify_token'] === token) {
+    console.log("Validating webhook");
+    res.status(200).send(req.query['hub.challenge']);
+  } else {
+    console.error("Failed validation. Make sure the validation tokens match.");
+    res.sendStatus(403);          
+  }  
+});
 
 app.post('/webhook', function (req, res) {
-    var data = req.body;
-  
-    // Make sure this is a page subscription
-    if (data.object === 'page') {
-  
-      // Iterate over each entry - there may be multiple if batched
-      data.entry.forEach(function(entry) {
-        var pageID = entry.id;
-        var timeOfEvent = entry.time;
-  
-        // Iterate over each messaging event
-        entry.messaging.forEach(function(event) {
-          if (event.message) {
-            receivedMessage(event);
-          } else {
-            console.log("Webhook received unknown event: ", event);
-          }
-        });
+  var data = req.body;
+
+  // Make sure this is a page subscription
+  if (data.object === 'page') {
+
+    // Iterate over each entry - there may be multiple if batched
+    data.entry.forEach(function(entry) {
+      var pageID = entry.id;
+      var timeOfEvent = entry.time;
+
+      // Iterate over each messaging event
+      entry.messaging.forEach(function(event) {
+        if (event.message) {
+          receivedMessage(event);
+        } else {
+          console.log("Webhook received unknown event: ", event);
+        }
       });
+    });
+
+    // Assume all went well.
+    //
+    // You must send back a 200, within 20 seconds, to let us know
+    // you've successfully received the callback. Otherwise, the request
+    // will time out and we will keep trying to resend.
+    res.sendStatus(200);
+  }
+});
+
+function receivedMessage(event) {
+    var senderID = event.sender.id;
+    var recipientID = event.recipient.id;
+    var timeOfMessage = event.timestamp;
+    var message = event.message;
   
-      // Assume all went well.
-      //
-      // You must send back a 200, within 20 seconds, to let us know
-      // you've successfully received the callback. Otherwise, the request
-      // will time out and we will keep trying to resend.
-      res.sendStatus(200);
+    console.log("Received message for user %d and page %d at %d with message:", 
+      senderID, recipientID, timeOfMessage);
+    console.log(JSON.stringify(message));
+  
+    var messageId = message.mid;
+  
+    var messageText = message.text;
+    var messageAttachments = message.attachments;
+  
+    if (messageText) {
+  
+      // If we receive a text message, check to see if it matches a keyword
+      // and send back the example. Otherwise, just echo the text we received.
+      switch (messageText) {
+        case 'generic':
+          sendGenericMessage(senderID);
+          break;
+  
+        default:
+          sendTextMessage(senderID, messageText);
+      }
+    } else if (messageAttachments) {
+      sendTextMessage(senderID, "Message with attachment received");
     }
-  });
-    
-  function receivedMessage(event) {
+}
+  
+  function sendTextMessage(recipientId, messageText) {
+    var messageData = {
+      recipient: {
+        id: recipientId
+      },
+      message: {
+        text: messageText
+      }
+    };
+  
+    callSendAPI(messageData);
+  }
+
+  
+
+function sendGenericMessage(recipientId, messageText) {
+    // To be expanded in later sections
+}
+  
+function receivedMessage(event) {
     // Putting a stub for now, we'll expand it in the following steps
     console.log("Message data: ", event.message);
+}
 
-  }
+
+
+
 
 app.listen(app.get('port'), function(){
     console.log('running on port', app.get('port'))
-    console.log("FB_VERIFY_TOKEN: " + token);
-    console.log("FB_ACCESS_TOKEN: " + access);
 })
