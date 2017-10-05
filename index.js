3adbf5206cb44eb3546e5b16528adeb9528c675d
ncbot17 <ncbot17@gmail.com>
2017-10-05 11:47:14 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -17,18 +17,8 @@ app.set('port', (process.env.PORT||5000));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.get('/', function(req,res){
-    res.send('Hello Youtube!')
+    res.send('Hello Youtube!');
 })
-/*
-app.get('/webhook/', function(req, res){
-    console.log(req.query);
-    if(req.query['hub.verify_token'] ===
-        'my_voice_is_my_password_verify_me'){
-            res.send(req.query['hub.challenge'])
-        }
-    res.send('No entry')
-})
-*/
 
 app.get('/webhook', function(req, res) {
   if (req.query['hub.mode'] === 'subscribe' &&
@@ -103,7 +93,7 @@ function receivedMessage(event) {
     }
 }
   
-  function sendTextMessage(recipientId, messageText) {
+function sendTextMessage(recipientId, messageText) {
     var messageData = {
       recipient: {
         id: recipientId
@@ -114,22 +104,81 @@ function receivedMessage(event) {
     };
   
     callSendAPI(messageData);
-  }
-
-  
-
-function sendGenericMessage(recipientId, messageText) {
-    // To be expanded in later sections
 }
+
+function sendGenericMessage(recipientId) {
+  var messageData = {
+    recipient: {
+      id: recipientId
+    },
+    message: {
+      attachment: {
+        type: "template",
+        payload: {
+          template_type: "generic",
+          elements: [{
+            title: "rift",
+            subtitle: "Next-generation virtual reality",
+            item_url: "https://www.oculus.com/en-us/rift/",               
+            image_url: "http://messengerdemo.parseapp.com/img/rift.png",
+            buttons: [{
+              type: "web_url",
+              url: "https://www.oculus.com/en-us/rift/",
+              title: "Open Web URL"
+            }, {
+              type: "postback",
+              title: "Call Postback",
+              payload: "Payload for first bubble",
+            }],
+          }, {
+            title: "touch",
+            subtitle: "Your Hands, Now in VR",
+            item_url: "https://www.oculus.com/en-us/touch/",               
+            image_url: "http://messengerdemo.parseapp.com/img/touch.png",
+            buttons: [{
+              type: "web_url",
+              url: "https://www.oculus.com/en-us/touch/",
+              title: "Open Web URL"
+            }, {
+              type: "postback",
+              title: "Call Postback",
+              payload: "Payload for second bubble",
+            }]
+          }]
+        }
+      }
+    }
+  };  
+
+  callSendAPI(messageData);
+}  
   
 function receivedMessage(event) {
     // Putting a stub for now, we'll expand it in the following steps
     console.log("Message data: ", event.message);
 }
 
+function callSendAPI(messageData) {
+  request({
+    uri: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: { access_token: access},
+    method: 'POST',
+    json: messageData
 
+  }, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var recipientId = body.recipient_id;
+      var messageId = body.message_id;
 
-
+      console.log("Successfully sent generic message with id %s to recipient %s", 
+        messageId, recipientId);
+    } else {
+      console.error("Unable to send message.");
+      console.error(response);
+      console.error(error);
+    }
+  });  
+}
 
 app.listen(app.get('port'), function(){
     console.log('running on port', app.get('port'))
